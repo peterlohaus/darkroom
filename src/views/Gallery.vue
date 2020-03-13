15fbcab5bf0e85c449929d6d46f774df32f044cb
Peter Lohaus <peterlohaus@outlook.de>
2020-03-13 16:44:41 +0100

    Gallery Router

---
+++ src/views/Gallery.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="gallery">
+    <h1>This is the Gallery</h1>
+    <img src="@/assets/tandem-x-visuals-FZOOxR2auVI-unsplash.jpg" alt="" />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+img {
+  width: 100%;
+}
+</style>
